8d2ad873b5ddd2a33ad82afb7726dac8c4ede5cf
MOHAMEDMESKINE1 <meskinemohameddev@gmail>
2024-08-17 22:11:40 +0100

    updated new-fetaure/posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -80,6 +80,7 @@ class PostController extends Controller
      */
     public function update(Request $request, Post $post)
     { 
+        // update function 
        // Validate request data
         $request->validate([
             'title' => ['required'],
